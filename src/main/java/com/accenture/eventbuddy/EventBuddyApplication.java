3c9c0eb730fd0987d5dc2d14f4cc707f61ae2dec
JanaLL <jana1101011@gmail.com>
2022-12-05 14:18:14 +0200

    Empty Spring project

---
+++ src/main/java/com/accenture/eventbuddy/EventBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.eventbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/eventbuddy/EventBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.eventbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
