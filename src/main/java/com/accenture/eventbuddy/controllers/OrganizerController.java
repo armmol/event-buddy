11e4fc41002750591073dbee206f26e22b1ead2c
Armaan Mollah <armaan552@gmail.com>
2022-12-09 02:15:46 +0300

    Organizer Controller class Update and import optimization of VisitorController.java

---
+++ src/main/java/com/accenture/eventbuddy/controllers/OrganizerController.java
@@ -0,0 +1,50 @@
+package com.accenture.eventbuddy.controllers;
+
+import com.accenture.eventbuddy.models.Organizer;
+import com.accenture.eventbuddy.services.OrganizerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "organizer")
+public class OrganizerController {
+
+    @Autowired
+    private OrganizerService organizerService;
+
+    //Add organizer POST
+    @RequestMapping(value = {"/addOrganizer"}, method = RequestMethod.POST)
+    public String saveOrganizer(@ModelAttribute("organizerForm") Organizer organizer) {
+        if (organizer != null) {
+            organizerService.storeOrganizer(organizer);
+            return "redirect:/showOrganizer/" + organizer.getOrganizerId();
+        } else return "redirect:/notFoundError";
+    }
+
+    //Delete organizer
+    @RequestMapping(value = {"/deleteOrganizer"}, method = RequestMethod.POST)
+    public String deleteOrganizer(@RequestParam Long organizerId) {
+        organizerService.deleteById(organizerId);
+        return "redirect:/organizerList";
+    }
+
+    //Show organizer list
+    @RequestMapping(value = {"organizerList"}, method = RequestMethod.GET)
+    public String organizers(Model model) {
+        List<Organizer> organizers = organizerService.all();
+        model.addAttribute("organizers", organizers);
+        return "organizerList";
+    }
+
+    //Show specific organizer
+    @RequestMapping(value = {"/showOrganizer/{organizerId}"}, method = RequestMethod.GET)
+    public String organizer(@PathVariable("organizerId") Long organizerId, Model model) {
+        Organizer organizer = organizerService.getById(organizerId);
+        model.addAttribute("organizer", organizer);
+        return "showOrganizer";
+    }
+}
--- src/main/java/com/accenture/eventbuddy/controllers/VisitorController.java
+++ src/main/java/com/accenture/eventbuddy/controllers/VisitorController.java
@@ -1,9 +1,7 @@
 package com.accenture.eventbuddy.controllers;
 
-import com.accenture.eventbuddy.models.Language;
 import com.accenture.eventbuddy.models.Visitor;
 import com.accenture.eventbuddy.services.VisitorService;
-import jakarta.el.EvaluationListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
